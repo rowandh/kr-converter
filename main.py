9ecf76fd332967c7bbb4fb7a09c39280426c57c8
Rowan de Haas <rowandh@users.noreply.github.com>
2025-02-24 10:51:06 +1300

    Retain directory structure

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils import find_files, extract_datetime_from_filename
 
 
 def main():
-    data_folder = Path(r"C:\Users\Work\Documents\Source\kr-converter\tests\data")
+    data_folder = Path(r"C:\Users\Work\Downloads\zips_handhistories-agent-red-all-dates")
 
     output_folder = data_folder.parent / f"{data_folder.name}_converted"
     output_folder.mkdir(exist_ok=True)
@@ -20,9 +20,11 @@ def main():
             corrected_timestamp = extract_datetime_from_filename(file)
             converted_content = parse(html_content, corrected_timestamp, "$")
 
-            output_filename = f"{file.stem}_converted.txt"
-            output_filepath = output_folder / output_filename
+            relative_path = file.relative_to(data_folder)
+            output_filepath = output_folder / relative_path
+            output_filepath = output_filepath.with_suffix(".txt")
 
+            output_filepath.parent.mkdir(parents=True, exist_ok=True)
             output_filepath.write_text(converted_content, encoding="utf-8")
 
             processed = processed + 1
